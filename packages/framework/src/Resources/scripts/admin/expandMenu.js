2803b3d2e714829580ffdac21216149c4f8c88ab
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2019-06-21 06:40:11 +0200

    design - js standards fix

--- packages/framework/src/Resources/scripts/admin/expandMenu.js
+++ packages/framework/src/Resources/scripts/admin/expandMenu.js
@@ -3,8 +3,8 @@
     Shopsys = window.Shopsys || {};
 
     Shopsys.register.registerCallback(function ($container) {
-        $container.filterAllNodes('.js-menu-switcher').on("click", function(){
-            $('body').toggleClass("menu-collapsed");
+        $container.filterAllNodes('.js-menu-switcher').on('click', function () {
+            $('body').toggleClass('menu-collapsed');
         });
     });
 
